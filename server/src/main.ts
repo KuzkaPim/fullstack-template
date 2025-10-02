01395c412c659ec85245f1c352330203310c0a3b
Kuzma <kuzkapimenov@gmail.com>
2025-10-02 20:09:46 +0500

    Базовая настройка сервера

---
+++ server/src/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
+
+async function bootstrap() {
+	const app = await NestFactory.create(AppModule);
+
+	app.use(cookieParser());
+
+	app.enableCors({
+		origin: process.env.BASE_CLIENT_URL ?? 'http://localhost:3000',
+		methods: 'GET, POST, PUT, PATCH, DELETE',
+		allowedHeaders: 'Content-Type',
+	});
+
+	app.useGlobalPipes(
+		new ValidationPipe({ transform: true, whitelist: true }),
+	);
+
+	await app.listen(process.env.PORT ?? 3000);
+}
+
+void bootstrap();
